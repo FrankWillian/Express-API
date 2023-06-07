ba404e413294c78832d35fcffcebc22a540b4501
Frank Willian <frank19valentim@gmail.com>
2023-06-07 13:28:42 -0300

    Implementado rota de get

--- index.js
+++ index.js
@@ -6,4 +6,23 @@ const app = express();
 
 app.use(express.json());
 
+app.post( "/books", (req, res) => {
+    const {id, title, author, publishedAt } = req.body;
+    const book = { id, title, author, publishedAt };
+    books.push(book);
+    return res.status(201).json(book)
+});
+
+app.get("/books", (req, res) => {
+    const allBooks = books;
+    return res.status(200).json(allBooks);
+});
+
+app.get("/books/:book_id", (req, res) => {
+    const { book_id } = req.params;
+    const book = books.find((book) => book.id === book_id);
+    if (!book) res.status(404).json("not found")
+    return res.status(200).json(book);
+});
+
 app.listen(3333, () => console.log("Server is runnig"))
